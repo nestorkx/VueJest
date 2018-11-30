277c6496c24fdf0346368de7184429cdf884ff7c
Nestor Kauil <nestor@cloudware.tech>
2018-11-30 12:26:01 -0500

    - Subiendo ejemplo basico de Vue y pruebas unitarias con Jest

---
+++ test/unit/specs/HelloWorld.spec.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld'
+
+describe('HelloWorld.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(HelloWorld)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .toEqual('Hello Vue :)')
+  })
+  test('Creando un snapshot con Vue', () => {
+    const Constructor = Vue.extend(HelloWorld)
+    const vm = new Constructor().$mount()
+    expect(vm.$el).toMatchSnapshot()
+  })
+})
